c5c49776601a0491814856f88e0b6434ab11756a
Ryzal Kamis <ryzalk.co@gmail.com>
2022-01-02 05:51:16 +0800

    Temporarily remove Kapitan Scout from guide.

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.src_package_name}}_api/run.sh
+++
@@ -1,7 +0,0 @@
-if [ -e env.sh ]; then
-    source env.sh
-fi
-if [ -e secret ]; then
-    source secret/secret.sh
-fi
-seldon-core-microservice $MODEL_NAME --service-type $SERVICE_TYPE --persistence $PERSISTENCE
